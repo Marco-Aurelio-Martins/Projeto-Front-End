a9ae7ebaf6e44f1be5c041593b0fd6401a3e311e
Marco Aurélio Ribeiro Martins <129920356+Marco-Aurelio-Martins@users.noreply.github.com>
2024-03-09 01:46:01 -0300

    Add files via upload

---
+++ projnew/script/paginas.js
@@ -0,0 +1,54 @@
+function busca() {
+    var input = document.getElementById("input");
+    var divgrande = document.getElementById("busca");
+    var lupa = document.getElementById("lupa");
+    var resultadoPesquisa = document.getElementById('resultadoPesquisa');
+
+    if (input.style.width === "200px") {
+        input.style.width = "0px";
+        divgrande.style.borderRadius = "25px";
+        lupa.style.transform = 'rotate(0deg)';
+        resultadoPesquisa.style.height = "0px";
+    } else {
+        divgrande.style.borderRadius = "25px";
+        input.style.width = "200px";
+        lupa.style.transform = 'rotate(-360deg)';
+        resultadoPesquisa.style.height = "auto";
+    }
+}
+
+
+var names= {
+    'Minecraft': '/html/minecraft.html',
+    'Diablo IV': '/html/diablo.html',
+}
+
+function checkEnter(event) {
+    if (event.keyCode === 13) { 
+        pesquisa();
+    }
+}
+
+function pesquisa() {
+    var input = document.getElementById('input').value.toUpperCase(); 
+    var resultadoPesquisa = document.getElementById('resultadoPesquisa');
+    resultadoPesquisa.innerHTML = ''; 
+
+    if (input.trim() === '') {
+        return;
+    }
+
+    for (var key in names) { 
+        if (key.toUpperCase().includes(input)) { 
+            var li = document.createElement('li'); 
+            var a = document.createElement('a');
+            a.href = names[key];
+            a.textContent = key;
+            li.appendChild(a);
+            resultadoPesquisa.appendChild(li); 
+        }
+    }   
+} 
+
+
+document.getElementById('input').addEventListener('input', pesquisa);
---
+++ projnew/script/script.js
@@ -0,0 +1,86 @@
+let count = 1;
+let i = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextimg();
+}, 5000)
+
+function nextimg(){
+    if(i<4){
+        count++;
+        i++;
+    }
+    else{
+        count--;
+        i++;
+        if(i == 8)
+        {
+            i = 0;
+        }
+        
+        
+    }
+
+    document.getElementById("radio"+count).checked = true
+}   
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 500)
+})
+
+function busca() {
+    var input = document.getElementById("input");
+    var divgrande = document.getElementById("busca");
+    var lupa = document.getElementById("lupa");
+    var resultadoPesquisa = document.getElementById('resultadoPesquisa');
+
+    if (input.style.width === "200px") {
+        input.style.width = "0px";
+        divgrande.style.borderRadius = "25px";
+        lupa.style.transform = 'rotate(0deg)';
+        resultadoPesquisa.style.height = "0px";
+    } else {
+        divgrande.style.borderRadius = "25px";
+        input.style.width = "200px";
+        lupa.style.transform = 'rotate(-360deg)';
+        resultadoPesquisa.style.height = "auto";
+    }
+}
+
+
+var names= {
+    'Minecraft': 'html/minecraft.html',
+    'Diablo IV': '/html/diablo.html',
+}
+
+function checkEnter(event) {
+    if (event.keyCode === 13) { 
+        pesquisa();
+    }
+}
+
+function pesquisa() {
+    var input = document.getElementById('input').value.toUpperCase(); 
+    var resultadoPesquisa = document.getElementById('resultadoPesquisa');
+    resultadoPesquisa.innerHTML = ''; 
+
+    if (input.trim() === '') {
+        return;
+    }
+
+    for (var key in names) { 
+        if (key.toUpperCase().includes(input)) { 
+            var li = document.createElement('li'); 
+            var a = document.createElement('a');
+            a.href = names[key];
+            a.textContent = key;
+            li.appendChild(a);
+            resultadoPesquisa.appendChild(li); 
+        }
+    }   
+} 
+
+
+document.getElementById('input').addEventListener('input', pesquisa);
