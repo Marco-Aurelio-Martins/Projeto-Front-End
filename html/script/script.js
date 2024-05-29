860dbc11eb5e077dbda31a478d9cd1685b6b4ce1
marco <129920356+Marco-Aurelio-Martins@users.noreply.github.com>
2024-05-29 17:51:25 +0000

    ajeitando o site

--- html/script/script.js
+++ html/script/script.js
@@ -51,8 +51,9 @@ function busca() {
 
 
 var names= {
-    'Minecraft': 'html/minecraft.html',
-    'Diablo IV': '/html/diablo.html',
+    'Minecraft': 'https://marco-aurelio-martins.github.io/Projeto-Front-End/html/minecraft.html',
+    'Diablo IV': 'https://marco-aurelio-martins.github.io/Projeto-Front-End/',
+    'Far Cry': 'https://ominous-space-yodel-rjrrw6rqx773xrgw-3000.app.github.dev/html/farcry6.html'
 }
 
 function checkEnter(event) {
